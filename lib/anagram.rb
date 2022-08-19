f8e29aa318240afbd5f24fdd8c070205b4bf8913
Kenneth Albert <gichuka@gmail.com>
2022-08-19 15:43:57 +0300

    Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+require "pry"
+class Anagram
+ attr_reader :word
+ def initialize word
+  @word=word
+ end
+
+ def match array
+ new_word=@word.chars.sort
+ filtered=array.filter{|el|el.chars.sort==new_word}
+ filtered
+ end
+  
+
+end
+
+listen = Anagram.new("listen")
+listen.match(%w[enlists google inlets banana])
+
+binding.pry
